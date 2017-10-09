1db4679d173c99117b63c87cb9cf7717b2220893
anselm-boox <anselm.ogbunugafor@booxmedia.com>
2017-10-09 19:36:04 +0300

    univ-sys: project skeleton created.

---
+++ src/main/java/com/ogb/anselm/univ/sys/App.java
@@ -0,0 +1,13 @@
+package com.ogb.anselm.univ.sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
