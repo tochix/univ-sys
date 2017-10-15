a6595e70b8cb635f8ae652f9609df418c30c6d78
anselm-boox <anselm.emma@gmail.com>
2017-10-15 07:21:59 +0300

    test: fix ProjectCourse unit test

--- src/test/java/com/ogb/anselm/univ/sys/ProjectCourseTest.java
+++ src/test/java/com/ogb/anselm/univ/sys/ProjectCourseTest.java
@@ -10,31 +10,21 @@ public class ProjectCourseTest extends TestCase {
 		assertEquals("Weight of project is different from the one provided.", 30, projectCourse.weightOfProject());
 	}
 	
-	public void testProjectCourseShouldThrowExceptionOnEmptyProjectWeight() throws CourseException, IOException {
-		try {
-			ProjectCourse projectCourse = new ProjectCourse(true, 2, 5, false, 26, "Applied Chemistry", 123245);
-			fail("Creating ProjectCourse without project weight does not throw exception");
-		} catch (CourseException e) {
-			assertEquals(9, e.getErrorCode());
-			assertEquals(e.getMessage(), "ProjectCourse needs the weight to be set.");
-		}
-	}
-	
 	public void testProjectCourseShouldThrowExceptionOnInvalidWeight() throws CourseException, IOException {
 		try {
 			ProjectCourse projectCourse = new ProjectCourse(true, 2, 5, false, 26, "Applied Chemistry", 123245, -30);
 			fail("Creating ProjectCourse with an invalid project weight does not throw exception");
 		} catch (CourseException e) {
-			assertEquals(10, e.getErrorCode());
-			assertEquals(e.getMessage(), "ProjectCourse needs the weight to be between 1 to 100.");
+			assertEquals(9, e.getErrorCode());
+			assertEquals(e.getMessage(), "ProjectCourse needs the weight to be set between 0 to 100");
 		}
 		
 		try {
 			ProjectCourse projectCourse = new ProjectCourse(true, 2, 5, false, 26, "Applied Chemistry", 123245, 101);
 			fail("Creating ProjectCourse with an invalid project weight does not throw exception");
 		} catch (CourseException e) {
-			assertEquals(10, e.getErrorCode());
-			assertEquals(e.getMessage(), "ProjectCourse needs the weight to be between 1 to 100.");
+			assertEquals(9, e.getErrorCode());
+			assertEquals(e.getMessage(), "ProjectCourse needs the weight to be set between 0 to 100");
 		}
 	}
 }
